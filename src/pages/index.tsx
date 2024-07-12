9d823d4e97bfaa8e1a3c7d5a8ae48660995a064a
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-11 21:44:20 -0300

    feat: creating initial next structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen items-center justify-center bg-zinc-800 text-white font-Graphie`}
+    >
+      <h1 className="text-8xl font-bold">Hello, world.</h1>
+    </main>
+  );
+}
