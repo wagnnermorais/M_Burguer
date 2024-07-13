b4e44d2cd6eba36f0b1accc9fb4290f99158f5f5
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-13 20:36:02 -0300

    feat: creating navigation database

---
+++ src/pages/api/db/Navigation.ts
@@ -0,0 +1,22 @@
+export const navigation = [
+  {
+    id: 1,
+    value: "About",
+    link: "/about"
+  },
+  {
+    id: 2,
+    value: "Products",
+    link: "/products"
+  },
+  {
+    id: 3,
+    value: "Contact",
+    link: "/contact"
+  },
+  {
+    id: 4,
+    value: "Reviews",
+    link: "/reviews"
+  },
+]
