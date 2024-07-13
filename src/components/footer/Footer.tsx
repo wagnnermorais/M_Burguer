aeae6cd8d9b8224d011e9a5e678879febb6e8ff5
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-13 20:37:15 -0300

    feat: creating footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import { GithubIcon, LinkedinIcon } from "../icons/Icons";
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center py-2 bg-zinc-950">
+      <div className="mt-4 text-center">
+        <p className="text-slate-300 my-2">
+          1155 103rd St, Miami Beach, Florida.
+        </p>
+        <p className="text-slate-300">&copy; 2024 Mburguer.</p>
+      </div>
+      <div className="flex items-center my-4 gap-4">
+        <div className="cursor-pointer ease duration-300">{LinkedinIcon}</div>
+        <div className="cursor-pointer ease duration-300">{GithubIcon}</div>
+      </div>
+    </footer>
+  );
+}
