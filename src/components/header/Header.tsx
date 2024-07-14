28c38ec1d096255e4e9ec0b69464df3c6cc8e374
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-14 15:34:58 -0300

    feat: adding Link component in logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { navigation } from "../../pages/api/db/Navigation";
 import { CartIcon, SunIcon } from "../icons/Icons";
 import NavItem from "./NavItem";
@@ -5,10 +6,14 @@ import NavItem from "./NavItem";
 export default function Header() {
   return (
     <header className="flex items-center justify-between mx-auto py-8 px-8 w-[80%]">
-      <div className="flex gap-1">
-        <h1 className="text-4xl font-bold tracking-tighter">Marque&apos;s</h1>
-        <span className="text-lg self-end tracking-wider italic">burguer</span>
-      </div>
+      <Link href={"/"}>
+        <div className="flex gap-1">
+          <h1 className="text-4xl font-bold tracking-tighter">Marque&apos;s</h1>
+          <span className="text-lg self-end tracking-wider italic">
+            burguer
+          </span>
+        </div>
+      </Link>
       <menu className="flex items-center gap-8 mr-24">
         {navigation.map((item) =>
           /* prettier-ignore */
