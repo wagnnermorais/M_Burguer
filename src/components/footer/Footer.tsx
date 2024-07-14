b83005ec7e7a8f5ea90069eafa7d5d2c1b78b5fe
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-14 15:34:31 -0300

    fix: fixing company name

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <p className="text-slate-300 my-2">
           1155 103rd St, Miami Beach, Florida.
         </p>
-        <p className="text-slate-300">&copy; 2024 Mburguer.</p>
+        <p className="text-slate-300">&copy; 2024 Marque&apos;s burguer.</p>
       </div>
       <div className="flex items-center my-4 gap-4">
         <div className="cursor-pointer ease duration-300">{LinkedinIcon}</div>
