1ee9d26eb32ceb57db764f3e441853e26551a654
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-14 20:23:02 -0300

    feat: adding carousel component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import Carousel from "@/components/page/Carousel";
 import Hero from "@/components/page/Hero";
 import Menu from "@/components/page/Menu";
 import Layout from "@/layout/Layout";
+import Image from "next/image";
+import { carousel } from "./api/db/Carousel";
 
 export default function Home() {
   return (
@@ -8,6 +11,21 @@ export default function Home() {
       <Layout>
         <Hero />
         <Menu />
+        <Carousel autoslide={true} interval={3500}>
+          {carousel.map((item) => (
+            <div
+              key={item.src}
+              className="relative w-full h-[80vh] flex-shrink-0"
+            >
+              <Image
+                src={item.src}
+                alt="Carousel image"
+                fill
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          ))}
+        </Carousel>
       </Layout>
     </main>
   );
