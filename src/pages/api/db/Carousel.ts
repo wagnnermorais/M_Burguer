fc1318e482916f95dfd4163d9c344ec0b5efc589
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-14 20:22:33 -0300

    feat: creating carousel db

---
+++ src/pages/api/db/Carousel.ts
@@ -0,0 +1,34 @@
+export const carousel = [
+  {
+    id: 1,
+    src: "/images/carousel/1.jpg"
+  },
+  {
+    id: 2,
+    src: "/images/carousel/2.jpg"
+  },
+  {
+    id: 3,
+    src: "/images/carousel/3.jpg"
+  },
+  {
+    id: 4,
+    src: "/images/carousel/4.jpg"
+  },
+  {
+    id: 5,
+    src: "/images/carousel/5.jpg"
+  },
+  {
+    id: 6,
+    src: "/images/carousel/6.jpg"
+  },
+  {
+    id: 7,
+    src: "/images/carousel/7.jpg"
+  },
+  {
+    id: 8,
+    src: "/images/carousel/8.jpg"
+  },
+]
