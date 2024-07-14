762019a9a19d288608663e1ba6b068d6952925aa
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-14 15:34:13 -0300

    feat: creating menu database

---
+++ src/pages/api/db/Menu.ts
@@ -0,0 +1,74 @@
+export const menu = [
+  {
+    id: 1,
+    name: "Classic Cheeseburger",
+    price: 28.99,
+    ingredients: ["Beef Patty", "Cheddar Cheese", "Lettuce", "Tomato", "Onion", "Pickles", "Ketchup", "Mustard"]
+  },
+  {
+    id: 2,
+    name: "Bacon Delight",
+    price: 30.99,
+    ingredients: ["Beef Patty", "Bacon", "Cheddar Cheese", "Lettuce", "Tomato", "Onion", "BBQ Sauce"]
+  },
+  {
+    id: 3,
+    name: "Veggie Paradise",
+    price: 29.49,
+    ingredients: ["Veggie Patty", "Lettuce", "Tomato", "Onion", "Pickles", "Avocado", "Vegan Mayo"]
+  },
+  {
+    id: 4,
+    name: "Spicy Jalapeño",
+    price: 29.99,
+    ingredients: ["Beef Patty", "Pepper Jack Cheese", "Jalapeños", "Lettuce", "Tomato", "Chipotle Sauce"]
+  },
+  {
+    id: 5,
+    name: "Mushroom Swiss",
+    price: 30.49,
+    ingredients: ["Beef Patty", "Swiss Cheese", "Mushrooms", "Lettuce", "Garlic Aioli"]
+  },
+  {
+    id: 6,
+    name: "BBQ Bacon Burger",
+    price: 31.99,
+    ingredients: ["Beef Patty", "Bacon", "Cheddar Cheese", "Onion Rings", "Lettuce", "BBQ Sauce"]
+  },
+  {
+    id: 7,
+    name: "Avocado Ranch",
+    price: 29.99,
+    ingredients: ["Chicken Patty", "Avocado", "Lettuce", "Tomato", "Onion", "Ranch Dressing"]
+  },
+  {
+    id: 8,
+    name: "Blue Cheese Bliss",
+    price: 30.49,
+    ingredients: ["Beef Patty", "Blue Cheese", "Lettuce", "Tomato", "Onion", "Blue Cheese Dressing"]
+  },
+  {
+    id: 9,
+    name: "Double Trouble",
+    price: 32.99,
+    ingredients: ["Double Beef Patty", "Cheddar Cheese", "Lettuce", "Tomato", "Onion", "Pickles", "Special Sauce"]
+  },
+  {
+    id: 10,
+    name: "Hawaiian Delight",
+    price: 30.99,
+    ingredients: ["Beef Patty", "Pineapple", "Ham", "Swiss Cheese", "Lettuce", "Teriyaki Sauce"]
+  },
+  {
+    id: 11,
+    name: "Buffalo Chicken",
+    price: 29.99,
+    ingredients: ["Chicken Patty", "Buffalo Sauce", "Lettuce", "Tomato", "Blue Cheese Dressing"]
+  },
+  {
+    id: 12,
+    name: "Mediterranean Veggie",
+    price: 29.49,
+    ingredients: ["Falafel Patty", "Lettuce", "Tomato", "Onion", "Cucumber", "Tzatziki Sauce"]
+  }
+]
