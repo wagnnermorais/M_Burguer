1e64b622873e36106f96d0cbd5b94b7cf893ee52
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-17 21:21:04 -0300

    refac: changing components order

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Carousel from "@/components/page/Carousel";
-import Hero from "@/components/page/Hero";
-import Menu from "@/components/page/menu/Menu";
 import Layout from "@/layout/Layout";
+import Carousel from "@/components/page/Carousel";
+import Banner from "@/components/page/Banner";
+import Menu from "@/components/page/menu/Menu";
 import Image from "next/image";
 import { carousel } from "./api/db/Carousel";
 
@@ -9,9 +9,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen font-Graphie bg-zinc-800 text-white">
       <Layout>
-        <Hero />
-        <Menu />
-        <Carousel autoslide={true} interval={3500}>
+        <Carousel autoslide={true} interval={4500}>
           {carousel.map((item) => (
             <div
               key={item.src}
@@ -26,6 +24,8 @@ export default function Home() {
             </div>
           ))}
         </Carousel>
+        <Menu />
+        <Banner />
       </Layout>
     </main>
   );
