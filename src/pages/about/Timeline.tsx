fcb6adf56f97933bab0874d1d40589330af56ae7
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-17 21:21:13 -0300

    feat: creating timeline component

---
+++ src/pages/about/Timeline.tsx
@@ -0,0 +1,40 @@
+import { timeline } from "../api/db/Timeline";
+
+export default function Timeline() {
+  return (
+    <section className="relative mb-12">
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-px bg-gray-400 h-full z-0"></div>
+      <div className="relative z-10">
+        {timeline.map((year, index) => (
+          <div
+            key={year.id}
+            className={`flex flex-col md:flex-row items-start md:items-center relative mb-0 ${
+              index % 2 === 0 ? "md:justify-start" : "md:justify-end"
+            }`}
+          >
+            <div
+              className={`w-full md:w-1/2 flex flex-col ${
+                index % 2 === 0
+                  ? "items-start text-left md:pr-8 pr-8"
+                  : "items-end text-right md:pl-8 pl-8"
+              }`}
+            >
+              <div className="p-6 text-justify rounded-lg bg-white text-black">
+                <h3
+                  className={`text-xl font-bold tracking-tighter mb-2 ${
+                    index % 2 === 0
+                      ? "items-start text-left"
+                      : "items-end text-right"
+                  }`}
+                >
+                  {year.year}
+                </h3>
+                <p>{year.text}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/api/db/Timeline.ts
@@ -0,0 +1,27 @@
+export const timeline = [
+  {
+    id: 1,
+    year: 2020,
+    text: "Started our journey with a passion for creating the best burgers in town. We spent countless hours perfecting our recipes and sourcing the freshest ingredients. Due to the COVID-19 pandemic, we began operating from our home garage, setting the foundation for our future success."
+  },
+  {
+    id: 2,
+    year: 2021,
+    text: "Launched our food truck, bringing our high-quality burgers to the community. Despite the challenges of the pandemic, our food truck quickly became a local favorite, known for its delicious and innovative burger creations."
+  },
+  {
+    id: 3,
+    year: 2022,
+    text: "Continued our success with the food truck and decided to expand. We introduced new menu items and participated in local events, further solidifying our presence in the community."
+  },
+  {
+    id: 4,
+    year: 2023,
+    text: "Opened our first brick-and-mortar location. We maintained our focus on quality and community, expanding our menu and introducing new flavors. We also started offering delivery services to reach more customers."
+  },
+  {
+    id: 5,
+    year: 2024,
+    text: "Expanded our menu to include seasonal specials and started eco-friendly packaging initiatives. We are working on new collaborations and community events to bring people together and celebrate our growth."
+  },
+]
