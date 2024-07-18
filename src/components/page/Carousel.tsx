30943687e3ebbf120679b2e4f09449820e8d1942
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-17 21:20:47 -0300

    style: changing font sizes, setting hover color and increasing transition time

--- src/components/page/Carousel.tsx
+++ src/components/page/Carousel.tsx
@@ -29,20 +29,20 @@ export default function Carousel({
   return (
     <div className="overflow-hidden relative h-[80vh]">
       <div
-        className="flex duration-500 transition-transform ease-in-out"
+        className="flex duration-1000 transition-transform ease-in-out"
         style={{ transform: `translateX(-${current * 100}%)` }}
       >
         {children}
       </div>
       <div className="absolute inset-0 flex items-center justify-between">
         <button
-          className="py-2 px-4 rounded-full shadow bg-white opacity-80 text-gray-800 hover:bg-white text-4xl"
+          className="mx-8 py-2 px-4 text-2xl rounded-full shadow opacity-80 ease duration-300 bg-white text-gray-800 hover:bg-zinc-600 hover:text-white"
           onClick={previousSlide}
         >
           &lt;
         </button>
         <button
-          className="py-2 px-4 rounded-full shadow bg-white opacity-80 text-gray-800 hover:bg-white text-4xl"
+          className="mx-8 py-2 px-4 text-2xl rounded-full shadow opacity-80 ease duration-300 bg-white text-gray-800 hover:bg-zinc-600 hover:text-white"
           onClick={nextSlide}
         >
           &gt;
