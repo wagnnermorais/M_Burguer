aa7458f5f5935cae0c920496301da35b9b331eee
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-22 21:58:34 -0300

    feat: creating review database

---
+++ src/pages/api/db/Reviews.ts
@@ -0,0 +1,44 @@
+export const reviews = [
+  {
+    id: 1,
+    user: "Dexter Morgan",
+    avatar: "/images/reviews/dexter.jpg",
+    review: "This place is incredible! The burgers are juicy and full of flavor. The staff is friendly, and the ambiance is perfect for a casual meal. Highly recommend!",
+    stars: 5,
+  },
+  {
+    id: 2,
+    user: "Harvey Specter",
+    avatar: "/images/reviews/harvey.jpg",
+    review: "Top-notch burgers with excellent service. The ambiance is stylish and comfortable. It's my go-to spot for a quick and satisfying meal.",
+    stars: 5,
+  },
+  {
+    id: 3,
+    user: "Walter White",
+    avatar: "/images/reviews/walter.jpg",
+    review: "The burgers here are incredible. Every bite is packed with flavor. The staff is friendly and attentive. Highly recommended!",
+    stars: 5,
+  },
+  {
+    id: 4,
+    user: "Jax Teller",
+    avatar: "/images/reviews/jax.jpg",
+    review: "Amazing burgers with a great atmosphere. The flavors are unique and delicious. Definitely coming back for more.",
+    stars: 5,
+  },
+  {
+    id: 5,
+    user: "Rick Grimes",
+    avatar: "/images/reviews/rick.jpg",
+    review: "Fantastic place for a great burger. The quality of the food is excellent, and the staff is very welcoming. Highly recommend!",
+    stars: 5,
+  },
+  {
+    id: 6,
+    user: "Leonardo DiCaprio",
+    avatar: "/images/reviews/leonardo.jpg",
+    review: "Good but not great. The burger was decent, but I've had better. The service was friendly, but the wait time was too long.",
+    stars: 4,
+  }
+]
