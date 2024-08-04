2fba23d96d9ee3b958bc518c37ad32c64aae32f5
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-08-04 20:10:43 -0300

    feat: creating review carousel component

---
+++ src/components/page/review/ReviewCarousel.tsx
@@ -0,0 +1,9 @@
+import Testimonial from "./Testimonial";
+
+export default function ReviewCarousel() {
+  return (
+    <div className="my-8 w-[80%] mx-auto">
+      <Testimonial />
+    </div>
+  );
+}
